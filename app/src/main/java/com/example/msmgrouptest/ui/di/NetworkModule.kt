a6ad63d55295420422e2de274fd162ca688ce148
KustovYuri <iurban.kustov.200@gmail.com>
2023-10-31 03:05:19 +1000

    Create Hillt NetworkModule with Retrofit

---
+++ app/src/main/java/com/example/msmgrouptest/ui/di/NetworkModule.kt
@@ -0,0 +1,67 @@
+package com.example.msmgrouptest.ui.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "hostName"
+
+    @Provides
+    @Singleton
+    @Named(BASE_URL)
+    fun provideBaseUrlString():String = "https://fefufit.dvfu.ru"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        @Named(BASE_URL) baseUrl:String
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(retrofit: Retrofit): MsmApi {
+        return retrofit.create(MsmApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptors: ArrayList<Interceptor>):OkHttpClient{
+        val clientBuilder = OkHttpClient.Builder()
+            .followRedirects(false)
+        interceptors.forEach{
+            clientBuilder.addInterceptor(it)
+        }
+        return clientBuilder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideInterceptors():ArrayList<Interceptor>{
+        val interceptors = arrayListOf<Interceptor>()
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        interceptors.add(loggingInterceptor)
+        return interceptors
+    }
+
+}
--- app/src/main/java/com/example/msmgrouptest/ui/sing_in/SingInScreen.kt
+++ app/src/main/java/com/example/msmgrouptest/ui/sing_in/SingInScreen.kt
@@ -2,7 +2,6 @@ package com.example.msmgrouptest.ui.sing_in
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -27,7 +26,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
