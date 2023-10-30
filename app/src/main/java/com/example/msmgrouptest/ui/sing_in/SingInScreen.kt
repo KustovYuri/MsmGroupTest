05b8dedf3e2157e44b6f609767c403cff2b01bae
KustovYuri <iurban.kustov.200@gmail.com>
2023-10-31 04:43:17 +1000

    add visibility password function in SingInscreen

--- app/src/main/java/com/example/msmgrouptest/ui/sing_in/SingInScreen.kt
+++ app/src/main/java/com/example/msmgrouptest/ui/sing_in/SingInScreen.kt
@@ -24,7 +24,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -33,6 +38,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -85,6 +91,15 @@ fun SingInScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun InputForm(viewModel: SingInScreenViewModel){
+
+    var passwordVisibility by remember { mutableStateOf(false) }
+
+    val icon = if(passwordVisibility){
+        painterResource(id = R.drawable.baseline_visibility_off_24)
+    }else{
+        painterResource(id = R.drawable.baseline_visibility_24)
+    }
+
     Row(
         modifier = Modifier.fillMaxWidth(0.81f),
         horizontalArrangement = Arrangement.Start
@@ -108,6 +123,10 @@ private fun InputForm(viewModel: SingInScreenViewModel){
         ),
         singleLine = true,
         textStyle = TextStyle(fontSize = 16.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = buttonTextColor,
+            cursorColor = buttonTextColor
+        )
     )
 
 
@@ -133,9 +152,21 @@ private fun InputForm(viewModel: SingInScreenViewModel){
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Password
         ),
-        visualTransformation = PasswordVisualTransformation(),
+        visualTransformation = if(passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
         singleLine = true,
         textStyle = TextStyle(fontSize = 16.sp),
+        trailingIcon = {
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(
+                    painter = icon,
+                    contentDescription = null,
+                )
+            }
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = buttonTextColor,
+            cursorColor = buttonTextColor
+        )
     )
     Spacer(modifier = Modifier.height(48.dp))
 }
